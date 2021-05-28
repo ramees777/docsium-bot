c417c8b72b40e7a1b2dc26e831354696bfed4779
docsploit <robertdevasia@outlook.com>
2021-05-28 22:33:12 +0530

    updated version

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM node:14.17.0
 ADD package.json /package.json
 RUN npm install
 
+ADD lib/provider.theme.js /lib/provider.theme.js
+
 
 ADD index.js /index.js
 
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { default: axios } = require("axios")
 const rx = /<!--Docsium::START-->[\s\S]*<!--Docsium::END-->/gi;
 const url = process.env.INPUT_WK_URL
 const wk_key = process.env.INPUT_WAKATIME_API_KEY
+const gh_token = process.env.INPUT_GH_TOKEN
+const octokit = require('@octokit/rest')
+const github = new octokit.Octokit({ auth: gh_token, });
+
 async function callWakaTime() {
 
     const encKey = Buffer.from(wk_key).toString('base64')
@@ -13,34 +17,34 @@ async function callWakaTime() {
 
     try {
         const lang_data = response['data']['languages']
-        return createStatusBar(lang_data)
+        const newCollections = createStatusBar(lang_data)
+        const content_ur = (await github.repos.getContent({ owner: 'docsploit', repo: 'docsium-bot', path: 'lib/template.svg' }))
+        const data = Buffer.from(content_ur.data.content, 'base64').toString('utf-8')
+        let new_readme = data.replace(rx, newCollections);
+        new_readme = Buffer.from(new_readme, 'utf-8').toString('base64')
+        await github.repos.createOrUpdateFileContents({ content: new_readme, owner: 'docsploit', message: 'Updated status', path: 'lib/template.svg', repo: 'docsium-bot', sha: content_ur.data.sha })
     }
     catch (error) {
         console.log(error);
-        console.log("Please Add your WakaTime API Key to the Repository Secrets")
+        // console.log("Please Add your WakaTime API Key to the Repository Secrets")
     }
 
 
 }
+
 
 callWakaTime()
 
 module.exports = callWakaTime
 
 const theme = require('./lib/provider.theme');
-const { readFileSync, writeFileSync } = require("fs");
-
 function createStatusBar(data) {
     const emmetString = []
     for (let i = 0; i < data.length; i++) {
         let new_theme = theme.Theme.change(data[i].name, color(data[i].name), data[i].percent, data[i].text, i)
         emmetString.push(new_theme)
     }
-    const svgFile = readFileSync(__dirname + '/lib/template.svg').toString('utf-8')
-    const fileMOS = svgFile.replace(rx, emmetString.toString())
-    console.log(svgFile===fileMOS);
-    writeFileSync(__dirname + '/lib/template.svg', fileMOS)
-    console.log('finished');
+    return emmetString;
 }
 
 
